53d46055c287d68a5d4ef130e9d9bd412e5b62ea
VIJAYA CHANDU SHREE <1BI20AI053@bit-bangalore.edu.in>
2024-10-23 13:49:48 +0530

    add to cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,8 +2,10 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem} from './CartSlice';
+import { useDispatch } from 'react-redux';
 
 function ProductList() {
+    const dispatch=useDispatch();
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
@@ -289,7 +291,7 @@ const handlePlantsClick = (e) => {
                 <div className="product-title">{plant.name}</div>
                 <div className='Product-discription'>{plant.description}</div>
                 <div className="product-price">{plant.cost}</div>
-                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                <button  className="product-button" onClick={() => handleAddToCart(plant)}>{addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}</button>
             </div>
             ))}
         </div>
